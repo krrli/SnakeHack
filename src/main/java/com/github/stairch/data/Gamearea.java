7ee6f3bf472bf37a6ae2b471b84edebc7965e891
Carla Iten <carlaiten@gmail.com>
2017-11-11 13:40:06 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/com/github/stairch/data/Gamearea.java
+++ src/main/java/com/github/stairch/data/Gamearea.java
@@ -12,8 +12,19 @@ public class Gamearea {
      * init every field of gamearea
      */
     public void initGameField(){
-
+        for(int i=0; i<gameArea.length; i++) {
+            for(int j=0; j<gameArea[i].length; j++) {
+                gameArea[i][j] = new GameField(0,0,false);
+                System.out.println("Values at arr["+i+"]["+j+"] is "+gameArea[i][j]);
+            }
+        }
     }
+
+    /*
+    public GameField[][] getGameArea(){
+        return this.gameArea;
+    }*/
+
 
     public GameField getField(int x, int y){
         return gameArea[y][x];
--- src/main/java/com/github/stairch/rest/SnakeService.java
+++ src/main/java/com/github/stairch/rest/SnakeService.java
@@ -4,12 +4,14 @@ import com.github.stairch.dtos.*;
 import com.github.stairch.types.HeadType;
 import com.github.stairch.types.Move;
 import com.github.stairch.types.TailType;
-import com.google.gson.Gson;
+import com.google.gson.*;
+
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.CopyOnWriteArrayList;
+
+import java.lang.reflect.Type;
 
 import static com.github.stairch.RestInPeace.BASE_URI;
 
@@ -20,6 +22,8 @@ public class SnakeService {
      * Used for json serialization/deserialization.
      */
     private final Gson gson = new Gson();
+    //private Gamearea gamearea;
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -34,7 +38,11 @@ public class SnakeService {
     public final Response start(final StartRequestDTO startRequestDTO) {
         System.out.println(startRequestDTO);
 
-
+        /*
+        if(gamearea == null){
+            gamearea = new Gamearea(startRequestDTO.getHeight(), startRequestDTO.getWidth());
+        }
+        */
 
         final StartResponseDTO startResponse = new StartResponseDTO();
         startResponse.setColor("black");
@@ -52,13 +60,50 @@ public class SnakeService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/move")
-    public final Response move(final MoveRequestDTO startRequestDTO) {
-        System.out.println(startRequestDTO);
+    public final Response move(final String startRequestDTO) {
+        //System.out.println(startRequestDTO);
+        /*
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jo = (JsonObject)jsonParser.parse(startRequestDTO);
+        //String bla = gson.toJson(startRequestDTO);
+        System.out.println(jo);
+*/
+
+        GsonBuilder gson_builder = new GsonBuilder();
+        gson_builder.registerTypeAdapter(
+                JsonElement.class,
+                new JsonDeserializer<JsonElement>() {
+                    @Override
+                    public JsonElement deserialize(JsonElement arg0,
+                                                   Type arg1,
+                                                   JsonDeserializationContext arg2)
+                            throws JsonParseException {
+
+                        return arg0;
+                    }
+                } );
+
+        Gson gson = gson_builder.create();
+        JsonElement element = gson.fromJson(startRequestDTO, JsonElement.class);
+        JsonObject object = element.getAsJsonObject();
+        System.out.println(object);
+
+        JsonElement snakes = object.get("snakes");
+        if(snakes!= null){
+            JsonElement bla = snakes.getAsJsonArray().get(Integer.parseInt("coords"));
+
+            //JsonElement coords = jsonArray.getAsJsonObject().get("coords");
+
+
+        }
 
         final MoveResponseDTO moveResponse = new MoveResponseDTO();
         moveResponse.setMove(Move.left);
+/*
+        Gamearea gamearea = new Gamearea(startRequestDTO.getHeight(), startRequestDTO.getWidth());
+        gamearea.initGameField();
 
-
+*/
         final String responseBody = gson.toJson(moveResponse);
         return Response.status(Response.Status.OK).entity(responseBody).build();
     }
