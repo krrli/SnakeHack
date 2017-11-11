70c12504aba488f2317a5ca17a48a715ba5d23e9
LENOVOYOGA\andrinlenovoyoga <andrin@buerli.info>
2017-11-11 11:51:04 +0100

    added isbusy to gamefield

--- src/main/java/com/github/stairch/data/GameField.java
+++ src/main/java/com/github/stairch/data/GameField.java
@@ -3,13 +3,15 @@ package com.github.stairch.data;
 public class GameField {
     private final int x;
     private final  int y;
+    private final boolean isBusy;
     private int h;
     private int g;
     private int f;
 
-    public GameField(int x, int y){
+    public GameField(int x, int y, boolean isBusy){
         this.x = x;
         this.y = y;
+        this.isBusy = isBusy;
     }
 
     public int getH() {
